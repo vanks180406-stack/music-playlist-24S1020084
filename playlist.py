90cf913cbc62d833ea7b5402a002eacfc2d8ffa8
vanks180406-stack <vanks180406@gmail.com>
2025-12-02 10:38:52 +0700

    Add feature/add-song function

--- playlist.py
+++ playlist.py
@@ -1,30 +1,19 @@
 songs = []
 
-def print_menu():
-    print("""
-==== MUSIC PLAYLIST CLI ====
-1. Thêm bài hát
-2. Xem danh sách phát
-3. Tìm bài theo ca sĩ
-4. Thoát chương trình
-""")
+def add_song(title, artist, duration):
+    song = {
+        "title": title,
+        "artist": artist,
+        "duration": duration
+    }
+    songs.append(song)
+    return song
 
-def main():
-    while True:
-        print_menu()
-        choice = input("Chọn (1-4): ").strip()
-
-        if choice == "1":
-            print("Chức năng 'Thêm bài hát' sẽ làm ở bước 3.")
-        elif choice == "2":
-            print("Chức năng 'Xem danh sách phát' sẽ làm ở bước 4.")
-        elif choice == "3":
-            print("Chức năng 'Tìm theo ca sĩ' sẽ làm ở bước 5.")
-        elif choice == "4":
-            print("Tạm biệt bé ❤️")
-            break
-        else:
-            print("Lựa chọn không hợp lệ.")
-
+# Ví dụ chạy thử
 if __name__ == "__main__":
-    main()
+    t = input("Tên bài hát: ")
+    a = input("Ca sĩ: ")
+    d = int(input("Thời lượng (giây): "))
+    
+    added = add_song(t, a, d)
+    print("Đã thêm:", added)
